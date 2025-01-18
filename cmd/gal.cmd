66263af8bac6a778bb14c7ffaab7a7523887148f
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-19 04:35:17 +0900

    modified `gal.cmd`.

--- cmd/gal.cmd
+++ cmd/gal.cmd
@@ -1,1 +1,11 @@
-@powershell Get-Alias %*
+@echo off
+
+rem PowerShell Get-Alias
+
+if "%~1%~3" equ "*" (
+    if "%~2" neq "" (
+        powershell Get-Alias -Definition %~2
+        exit /b 0
+    )
+)
+powershell Get-Alias %*
