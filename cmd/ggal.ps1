d03d57d03248b4465d24c8eeda5f8ecd422a507d
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-19 05:40:22 +0900

    modified `ggal.ps1`.

--- cmd/ggal.ps1
+++ cmd/ggal.ps1
@@ -5,13 +5,17 @@
 param (
     [Parameter(Position=0)]
     [String]
-    $Alias
+    $Alias = '',
+    [Parameter(Position=1)]
+    [String]
+    $Content = ''
 )
 
-if ($Alias -eq '') { $Alias = '\w+' } else { $Alias = $Alias.Replace('*', '\w*') }
+if ($Alias   -eq '') { $Alias   = '\w+' } else { $Alias   = $Alias.Replace('*', '\w*')  }
+if ($Content -eq '') { $Content = '.*'  } else { $Content = $Content.Replace('*', '.*') }
 
 git config list --global |
-    Select-String "^alias\.($Alias)=(.*)" |
+    Select-String "^alias\.($Alias)=($Content)" |
     ForEach-Object {
         [PSCustomObject]@{
             Alias   = $_.Matches[0].Groups[1]
