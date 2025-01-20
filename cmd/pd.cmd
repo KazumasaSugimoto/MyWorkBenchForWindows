d0cf37a1c68199e5c8b78b75919290243a79cfd4
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-20 21:52:31 +0900

    modified `pd.cmd`.

--- cmd/pd.cmd
+++ cmd/pd.cmd
@@ -7,12 +7,29 @@ if "%~1" equ "" (
     exit /b 0
 )
 
+if "%~1" equ "." (
+    echo --- current directory tagging mode ---
+    if "%~2" equ "" (
+        echo usage:
+        echo     %~n0[%~x0] . tag-name
+        exit /b 1
+    )
+    set PD_TAGs[%~2]=%CD%
+    set PD_TAGs[%~2] | findstr /b /i /l "PD_TAGs[%~2]="
+    exit /b 0
+)
+
 setlocal EnableDelayedExpansion
 
 set DESTINATION=%~1
 
 if not exist "%DESTINATION%" (
-    if defined %DESTINATION% set DESTINATION=!%DESTINATION%!
+    if defined PD_TAGs[%DESTINATION%] (
+        set DESTINATION=!PD_TAGs[%DESTINATION%]!
+    )
+    if defined %DESTINATION% (
+        set DESTINATION=!%DESTINATION%!
+    )
 )
 
 endlocal & pushd "%DESTINATION%"
