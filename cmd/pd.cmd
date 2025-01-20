28f72ccaa272e80b1258acc2d2023d70dac5988c
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-20 21:42:10 +0900

    modified `pd.cmd`.

--- cmd/pd.cmd
+++ cmd/pd.cmd
@@ -1,4 +1,18 @@
 @echo off
+
 rem pushd/popd helper
-if "%~1" equ "" popd & goto :EOF
-pushd "%~1"
+
+if "%~1" equ "" (
+    popd
+    exit /b 0
+)
+
+setlocal EnableDelayedExpansion
+
+set DESTINATION=%~1
+
+if not exist "%DESTINATION%" (
+    if defined %DESTINATION% set DESTINATION=!%DESTINATION%!
+)
+
+endlocal & pushd "%DESTINATION%"
