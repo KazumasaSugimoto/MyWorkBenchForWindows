ed6cb61d09f74379461a9f7b9693c9d3bf75abf7
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-25 01:38:30 +0900

    added `echox.cmd`.

---
+++ cmd/echox.cmd
@@ -0,0 +1,68 @@
+@echo off
+
+rem Outputs all sort orders of arguments.
+
+for /f "usebackq tokens=*" %%a in (`echov.cmd --edit-self -es /es :es`) do if /i "%~1" equ "%%a" goto EDIT_SELF
+
+setlocal EnableDelayedExpansion
+
+set ARGS.COUNT=0
+
+:SET_ARGS
+
+if "%~1" equ "" goto DO_MAIN
+
+set /a ARGS.COUNT+=1
+set ARGS[%ARGS.COUNT%]=%~1
+
+shift /1
+goto SET_ARGS
+
+:DO_MAIN
+
+if %ARGS.COUNT% equ 0 goto SHOW_HELP
+
+for /f "usebackq tokens=*" %%a in (`gaso.cmd %ARGS.COUNT%`) do call :SUB_MAIN "%%a"
+exit /b 0
+
+:SUB_MAIN
+
+set RESULT=
+for /f "usebackq tokens=*" %%i in (`echov.cmd %~1`) do (
+    set RESULT=!RESULT! !ARGS[%%i]!
+)
+echo.%RESULT:~1%
+goto :EOF
+
+:EDIT_SELF
+
+call code.cmd "%~f0"
+exit /b 0
+
+:SHOW_HELP
+
+echo -------------------------------------------------------------------------------
+echo Outputs all sort orders of arguments.
+echo -------------------------------------------------------------------------------
+echo.
+echo usage:
+echo.
+echo     %~n0[%~x0] arguments...
+echo.
+echo example:
+echo.
+echo     %~n0 a b
+echo.
+echo         a b
+echo         b a
+echo.
+echo     %~n0 a b c
+echo.
+echo         a b c
+echo         a c b
+echo         b a c
+echo         b c a
+echo         c a b
+echo         c b a
+
+exit /b 0
