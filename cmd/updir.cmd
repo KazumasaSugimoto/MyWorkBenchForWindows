a3434369a2b2dd691f1568895d0d7e003f40884a
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-26 06:01:45 +0900

    added `updir.cmd`.

---
+++ cmd/updir.cmd
@@ -0,0 +1,45 @@
+@echo off
+
+if "%~1" equ "" goto SHOW_HELP
+for /f "usebackq tokens=*" %%a in (`echov.cmd --edit-self -es /es :ed`) do if /i "%~1" equ "%%a" goto EDIT_SELF
+
+setlocal EnableDelayedExpansion
+
+set RAW_ARG1=%~1
+if "%RAW_ARG1%" equ "%RAW_ARG1:\=%" (
+    set BASE_FOLDER=%CD%
+    set SCAN_PATTERN=%RAW_ARG1%
+) else (
+    set BASE_FOLDER=%~dp1
+    set SCAN_PATTERN=!RAW_ARG1:%~dp1=!
+)
+pushd "%BASE_FOLDER%"
+
+:SCAN_CURRENT_FOLDER
+
+for /f "usebackq tokens=*" %%a in (`dir /a-d /b "%SCAN_PATTERN%" 2^>nul`) do echo %CD%\%%a
+
+set SCANED_DIR=%CD%
+cd ..
+
+if "%SCANED_DIR%" neq "%CD%" goto SCAN_CURRENT_FOLDER
+
+popd
+exit /b 0
+
+:EDIT_SELF
+
+call code.cmd "%~f0"
+exit /b 0
+
+:SHOW_HELP
+
+echo -------------------------------------------------------------------------------
+echo Upstream Dir - the opposite of `dir /s`
+echo -------------------------------------------------------------------------------
+echo.
+echo usage:
+echo.
+echo     %~n0[%~x0] [path]file-name
+
+exit /b 1
