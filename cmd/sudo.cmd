03f077aad98e7701a05f2f9a7b95262d2ca68714
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-01-27 22:26:57 +0900

    modified `sudo.cmd`.

--- cmd/sudo.cmd
+++ cmd/sudo.cmd
@@ -3,15 +3,15 @@
 setlocal
 
 set HELP_OPTION=--help /help -? /?
-set EDIT_OPTION=--edit /edit --code /code --source /source
+set EDIT_OPTION=--edit-self -es /es :es
 
 for /f "usebackq tokens=1" %%a in (`echov %HELP_OPTION%`) do if /i "%%a" equ "%~1" goto :SHOW_HELP
 for /f "usebackq tokens=1" %%a in (`echov %EDIT_OPTION%`) do if /i "%%a" equ "%~1" goto :EDIT_SELF
 
 set TARGET_PROCESS=
-for /f "usebackq tokens=1" %%a in (`echov cmd`)     do if /i "%%a" equ "%~1" set TARGET_PROCESS=cmd.exe
-for /f "usebackq tokens=1" %%a in (`echov ps5 wps`) do if /i "%%a" equ "%~1" set TARGET_PROCESS=powershell.exe
-for /f "usebackq tokens=1" %%a in (`echov ps7 ps`)  do if /i "%%a" equ "%~1" set TARGET_PROCESS=pwsh.exe
+for /f "usebackq tokens=1" %%a in (`echov cmd`)     do if /i "%%a" equ "%~1" set TARGET_PROCESS=cmd.exe -ArgumentList '/t:5F /k """cd /d %CD%"""'
+for /f "usebackq tokens=1" %%a in (`echov ps5 wps`) do if /i "%%a" equ "%~1" set TARGET_PROCESS=powershell.exe -ArgumentList '-NoExit -Command cd """%CD%"""'
+for /f "usebackq tokens=1" %%a in (`echov ps7 ps`)  do if /i "%%a" equ "%~1" set TARGET_PROCESS=pwsh.exe -ArgumentList '-NoExit -Command cd """%CD%"""'
 
 if defined TARGET_PROCESS goto LAUNCH_TARGET_PROCESS
 
@@ -36,11 +36,11 @@ echo     %~n0[%~x0] ^<sub-command^>
 echo.
 echo SUB-COMMANDS:
 echo     cmd
-echo         Launch Command Prompt `cmd.exe`
+echo         Start Command Prompt `cmd.exe`
 echo     ps5 ^| wps
-echo         Launch Windows PowerShell `powershell.exe`
+echo         Start Windows PowerShell `powershell.exe`
 echo     ps7 ^| ps
-echo         PowerShell `pwsh.exe`
+echo         Start PowerShell `pwsh.exe`
 echo     %HELP_OPTION: = ^| %
 echo         Show Help
 echo     %EDIT_OPTION: = ^| %
@@ -50,4 +50,5 @@ exit /b 0
 
 :EDIT_SELF
 
-code "%~f0"
+call code.cmd "%~f0"
+exit /b 0
