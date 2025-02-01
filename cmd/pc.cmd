cf74dcecf2c0830ecef14511589edc743e0c144b
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-02 04:30:07 +0900

    added `pc.cmd`.

---
+++ cmd/pc.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+for /f "usebackq tokens=*" %%a in (`echov.cmd --edit-self -es /es :es`) do if /i "%~1" equ "%%a" goto EDIT_SELF
+
+setlocal
+
+set SUBCMDS_POWEROFF=poweroff off shutdown down
+set SUBCMDS_SUSPEND=suspend sleep zzz
+set SUBCMDS_REBOOT=reboot
+set SUBCMDS_LOGOUT=logout signout out logoff
+
+set SHUTDOWN_OPTIONS=
+
+for /f "usebackq tokens=*" %%a in (`echov.cmd %SUBCMDS_POWEROFF%`) do if /i "%~1" equ "%%a" set SHUTDOWN_OPTIONS=-s -hybrid -t 0
+for /f "usebackq tokens=*" %%a in (`echov.cmd %SUBCMDS_SUSPEND%` ) do if /i "%~1" equ "%%a" set SHUTDOWN_OPTIONS=-h
+for /f "usebackq tokens=*" %%a in (`echov.cmd %SUBCMDS_REBOOT%`  ) do if /i "%~1" equ "%%a" set SHUTDOWN_OPTIONS=-r -t 0
+for /f "usebackq tokens=*" %%a in (`echov.cmd %SUBCMDS_LOGOUT%`  ) do if /i "%~1" equ "%%a" set SHUTDOWN_OPTIONS=-l
+
+if defined SHUTDOWN_OPTIONS (
+    shutdown %SHUTDOWN_OPTIONS%
+    exit /b 0
+)
+
+echo -------------------------------------------------------------------------------
+echo `shutdown` command helper
+echo -------------------------------------------------------------------------------
+echo.
+echo usage:
+echo.
+echo     %~n0[%~x0] sub-command
+echo.
+echo sub-command;
+echo.
+echo     %SUBCMDS_POWEROFF: = ^| %
+echo     %SUBCMDS_SUSPEND: = ^| %
+echo     %SUBCMDS_REBOOT: = ^| %
+echo     %SUBCMDS_LOGOUT: = ^| %
+echo.
+
+:SELECT_SUBCMD
+
+set ANS_SUBCMD=
+set /p ANS_SUBCMD="? > "
+if defined ANS_SUBCMD call %~nx0 %ANS_SUBCMD%
+exit /b 0
+
+:EDIT_SELF
+
+call code.cmd "%~f0"
+exit /b 0
