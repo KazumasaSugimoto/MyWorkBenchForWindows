e4326920e10ea3786ad282d2f6b4cc5f3517a36e
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-02 02:27:03 +0900

    modified `echov.cmd`.

--- cmd/echov.cmd
+++ cmd/echov.cmd
@@ -1,3 +1,8 @@
+::  main uses:
+::      Treat wildcard symbols ('*','?') as charcters in the `for` command.
+::  example:
+::      NG: for %%a in ( --help -? /? ) do if /i "%~1" equ "%%a" ...
+::      OK: for /f "usebackq tokens=*" %%a in (`echov.cmd --help -? /?`) do if /i "%~1" equ "%%a" ...
 :LOOP
 @if "%~1" equ "" exit /b 0
 @echo.%~1
