805841a0a0afa9b6dbac20bed69c119efd91a567
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-02 10:18:05 +0900

    modified `profile.ps1`.

--- pss/profile.ps1
+++ pss/profile.ps1
@@ -1,4 +1,40 @@
 function prompt {
+
 #   "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
     "PS $($executionContext.SessionState.Path.CurrentLocation)`n> ";
+
+}
+
+function ver {
+
+<#
+    .SYNOPSIS
+        Put PSVersion String.
+#>
+
+    $PSVersionTable.PSVersion.ToString()
+
+}
+
+function pd {
+
+<#
+    .SYNOPSIS
+        pushd/popd helper.
+#>
+
+    param
+    (
+        [String]$Path = ''
+    )
+
+    if ($Path -ne '')
+    {
+        Push-Location -Path $Path   
+    }
+    else
+    {
+        Pop-Location
+    }
+
 }
