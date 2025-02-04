e36e98aaf561bf107479b083f6695cd5e0265dd3
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-04 23:43:04 +0900

    modified `drive-list.cmd`.

--- cmd/drive-list.cmd
+++ cmd/drive-list.cmd
@@ -4,7 +4,7 @@ rem Outputs assigned drive list.
 
 for /f "usebackq tokens=*" %%a in (`echor.cmd --edit-self -es /es :es`) do if /i "%~1" equ "%%a" goto EDIT_SELF
 
-for %%a in ( A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ) do (
+for /f "usebackq tokens=*" %%a in (`echov.cmd ABCDEFGHIJKLMNOPQRSTUVWXYZ`) do (
     pushd "%%a:\" 2>nul
     if not ERRORLEVEL 1 (
         echo %%a:
