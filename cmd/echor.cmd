d1f728b8449160234bbab52733b09e9177791635
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-04 22:09:29 +0900

    modified `echor.cmd`.

--- cmd/echor.cmd
+++ cmd/echor.cmd
@@ -1,8 +1,9 @@
+::  Echo each argument on a row.
 ::  main uses:
 ::      Treat wildcard symbols ('*','?') as charcters in the `for` command.
 ::  example:
 ::      NG: for %%a in ( --help -? /? ) do if /i "%~1" equ "%%a" ...
-::      OK: for /f "usebackq tokens=*" %%a in (`echov.cmd --help -? /?`) do if /i "%~1" equ "%%a" ...
+::      OK: for /f "usebackq tokens=*" %%a in (`echor.cmd --help -? /?`) do if /i "%~1" equ "%%a" ...
 :LOOP
 @if "%~1" equ "" exit /b 0
 @echo.%~1
