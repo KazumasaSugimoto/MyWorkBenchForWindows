3f10d9e9a26b8c36f7a772618d709594f70a24a7
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-05 23:58:44 +0900

    modified `drive-list.cmd`.

--- cmd/drive-list.cmd
+++ cmd/drive-list.cmd
@@ -9,20 +9,35 @@ setlocal
 set WITH_CURDIR=
 for /f "usebackq tokens=*" %%a in (`echor.cmd --cur-dir -cd /cd :cd`) do if /i "%~1" equ "%%a" set WITH_CURDIR=True
 
-set FOREACH_CMD=powershell "Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceId, Description | Format-Table -HideTableHeaders"
-for /f "usebackq tokens=1*" %%a in (`%FOREACH_CMD%`) do (
-    call :PUT_DRIVE_INFO "%%a" "%%b"
+set FOREACH_CMD=powershell "Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceId, DriveType, Description | Format-Table -HideTableHeaders"
+for /f "usebackq tokens=1,2*" %%a in (`%FOREACH_CMD%`) do (
+    call :PUT_DRIVE_INFO "%%a" "%%b" "%%c"
 )
 exit /b 0
 
 :PUT_DRIVE_INFO
 
 set DEVICE_ID=%~1
-set DESCRIPTION=%~2
+set DRIVE_TYPE=%~2
+set DESCRIPTION=%~3
+
+call :UPDATE_DESCRIPTION_OF_DRIVE_TYPE_%DRIVE_TYPE%
+
 set CURDIR_OR_ERRMSG=
 if defined WITH_CURDIR call :GET_SET_CURRENT_DIRECTORY
 
 echo %DEVICE_ID% %DESCRIPTION% %CURDIR_OR_ERRMSG%
+goto :EOF
+
+:UPDATE_DESCRIPTION_OF_DRIVE_TYPE_2     ::Removable
+:UPDATE_DESCRIPTION_OF_DRIVE_TYPE_5     ::CD Drive
+
+goto :EOF
+
+:UPDATE_DESCRIPTION_OF_DRIVE_TYPE_3     ::HDD
+:UPDATE_DESCRIPTION_OF_DRIVE_TYPE_4     ::Network Drive
+
+for /f "usebackq tokens=*" %%a in (`aldp.cmd "%DEVICE_ID%\" 2^>nul`) do set DESCRIPTION=%%a
 goto :EOF
 
 :GET_SET_CURRENT_DIRECTORY
