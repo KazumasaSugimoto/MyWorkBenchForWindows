e358af84b6040863608f1deead25911700645493
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-05 23:31:49 +0900

    modified `drive-list.cmd`.

--- cmd/drive-list.cmd
+++ cmd/drive-list.cmd
@@ -8,9 +8,29 @@ setlocal
 
 set FOREACH_CMD=powershell "Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceId, Description | Format-Table -HideTableHeaders"
 for /f "usebackq tokens=1*" %%a in (`%FOREACH_CMD%`) do (
-    echo %%a %%b
+    call :PUT_DRIVE_INFO "%%a" "%%b"
 )
 exit /b 0
+
+:PUT_DRIVE_INFO
+
+set DEVICE_ID=%~1
+set DESCRIPTION=%~2
+call :GET_SET_CURRENT_DIRECTORY
+
+echo %DEVICE_ID% %DESCRIPTION% %CURDIR_OR_ERRMSG%
+goto :EOF
+
+:GET_SET_CURRENT_DIRECTORY
+
+set CURDIR_OR_ERRMSG=
+for /f "usebackq tokens=*" %%e in (`pushd "%DEVICE_ID%" 2^>^&1`) do set CURDIR_OR_ERRMSG=%%e
+if defined CURDIR_OR_ERRMSG goto :EOF
+
+pushd "%DEVICE_ID%"
+set CURDIR_OR_ERRMSG=%CD%
+popd
+goto :EOF
 
 :EDIT_SELF
 
