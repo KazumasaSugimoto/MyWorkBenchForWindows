e6b17b8bd98eb95b8160404dcc22791d78b69b6d
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-09 04:41:10 +0900

    modified `wbh.sql`.

--- cmd/conf/wbh.sql
+++ cmd/conf/wbh.sql
@@ -1,1 +1,10 @@
-SELECT datetime(last_visit_time / 1000000 - 11644473600, 'unixepoch', 'localtime') AS last_visit_time, title, url FROM urls ORDER BY last_visit_time DESC;
+-- Web Browse History
+SELECT
+    datetime(last_visit_time / 1000000 - 11644473600, 'unixepoch', 'localtime') AS  last_visit_time
+,   title
+,   url
+FROM
+    urls
+ORDER BY
+    last_visit_time DESC
+;
