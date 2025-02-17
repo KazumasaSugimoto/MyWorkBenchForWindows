08075a691044bde01aea98a3a901a3c622b45c75
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-17 23:23:46 +0900

    added `ggbh.ps1`.

---
+++ cmd/ggbh.ps1
@@ -0,0 +1,53 @@
+using namespace System.IO
+using namespace System.Security.Cryptography
+using namespace System.Text
+
+<#
+    Get Git Blob Hash
+#>
+param
+(
+    [Parameter(Position=0)]
+    [String]
+    $FilePath
+)
+
+[MemoryStream]$memoryStream = $null
+[FileStream]$fileStream = $null
+
+try
+{
+    $fileInfo = [FileInfo]::new($FilePath)
+    $gitBlobHeaderBytes = [Encoding]::ASCII.GetBytes("blob $($FileInfo.Length)")
+
+    $memoryStream = [MemoryStream]::new()
+    $memoryStream.Write($gitBlobHeaderBytes, 0, $gitBlobHeaderBytes.Length)
+    $memoryStream.WriteByte(0x00)
+
+    $fileStream = $fileInfo.OpenRead()
+    $fileStream.CopyTo($memoryStream)
+
+    [void]$memoryStream.Seek(0, [SeekOrigin]::Begin)
+
+    $hashProvider = [SHA1CryptoServiceProvider]::new()
+    $gitBlobHash = $hashProvider.ComputeHash($memoryStream)
+
+    Write-Output ([BitConverter]::ToString($gitBlobHash).ToLower() -replace '-', '')
+}
+catch
+{
+    Throw
+}
+finally
+{
+    if ($null -ne $fileStream)
+    {
+        $fileStream.Dispose()
+        $fileStream = $null
+    }
+    if ($null -ne $memoryStream)
+    {
+        $memoryStream.Dispose()
+        $memoryStream = $null
+    }
+}
