669ba395553c69ddcc2d8f43e7c3bcb56ba97a3b
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-18 22:20:18 +0900

    added `ps.cmd`.

---
+++ cmd/ps.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+rem PowerShell Invoker
+
+:TRY_DEFAULT
+
+if not defined MYDEF_PSEXE goto TRY_NEW_VERSION
+
+where %MYDEF_PSEXE% >nul 2>&1
+if ERRORLEVEL 1 goto TRY_NEW_VERSION
+
+%MYDEF_PSEXE% %*
+exit /b %ERRORLEVEL%
+
+:TRY_NEW_VERSION
+
+where pwsh >nul 2>&1
+if ERRORLEVEL 1 goto USE_OLD_VERSION
+
+pwsh %*
+exit /b %ERRORLEVEL%
+
+:USE_OLD_VERSION
+
+powershell %*
+exit /b %ERRORLEVEL%
