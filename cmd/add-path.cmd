49cc23370251a9ea6ec1e24abcaa79b8e62814ac
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-20 22:49:29 +0900

    modified `add-path.cmd`.

--- cmd/add-path.cmd
+++ cmd/add-path.cmd
@@ -16,6 +16,13 @@ if not exist "%~f1" (
     exit /b 1
 )
 
+for /f "usebackq tokens=*" %%a in (`ps.cmd -Command "(Get-Item -Path '%~f1').Mode -like 'd*'"`) do (
+    if /i "%%a" neq "true" (
+        echo not folder.>&2
+        exit /b 1
+    )
+)
+
 call del-path.cmd "%~f1" 2>nul
 
 set PATH=%~f1;%PATH%
