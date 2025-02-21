02c6acebdfac34b2cb5c7520776ab97335ff45e8
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-22 00:04:21 +0900

    modified `echox.cmd`.

--- cmd/echox.cmd
+++ cmd/echox.cmd
@@ -1,8 +1,31 @@
 @echo off
 
+rem -------------------------------------------------------------------------------
 rem Outputs all sort orders of arguments.
+rem -------------------------------------------------------------------------------
+rem usage:
+rem     echox[.cmd] arguments...
+rem example:
+rem     case1:
+rem         echox a b
+rem             result:
+rem                 a b
+rem                 b a
+rem     case2:
+rem         echox a b c
+rem             result:
+rem                 a b c
+rem                 a c b
+rem                 b a c
+rem                 b c a
+rem                 c a b
+rem                 c b a
 
-if "%~1" equ "" goto SHOW_HELP
+if "%~1" equ "" (
+    call bshelp.cmd "%~f0"
+    exit /b 1
+)
+
 for /f "usebackq tokens=*" %%a in (`echor.cmd --edit-self -es /es :es`) do if /i "%~1" equ "%%a" goto EDIT_SELF
 
 setlocal EnableDelayedExpansion
@@ -35,31 +58,3 @@ goto :EOF
 
 call code.cmd "%~f0"
 exit /b 0
-
-:SHOW_HELP
-
-echo -------------------------------------------------------------------------------
-echo Outputs all sort orders of arguments.
-echo -------------------------------------------------------------------------------
-echo.
-echo usage:
-echo.
-echo     %~n0[%~x0] arguments...
-echo.
-echo example:
-echo.
-echo     %~n0 a b
-echo.
-echo         a b
-echo         b a
-echo.
-echo     %~n0 a b c
-echo.
-echo         a b c
-echo         a c b
-echo         b a c
-echo         b c a
-echo         c a b
-echo         c b a
-
-exit /b 1
