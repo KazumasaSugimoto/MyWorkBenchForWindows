54d5d1874eb6d05703553fb3c66cedaff876a6fb
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-02-22 00:16:57 +0900

    modified `gaso.cmd`.

--- cmd/gaso.cmd
+++ cmd/gaso.cmd
@@ -1,6 +1,25 @@
 @echo off
 
+rem -------------------------------------------------------------------------------
 rem Generate All Sort Orders
+rem -------------------------------------------------------------------------------
+rem usage:
+rem     gaso[.cmd] number-of-elements
+rem example:
+rem     case1:
+rem         gaso 2
+rem             result:
+rem                 1 2
+rem                 2 1
+rem     case2:
+rem         gaso 3
+rem             result:
+rem                 1 2 3
+rem                 1 3 2
+rem                 2 1 3
+rem                 2 3 1
+rem                 3 1 2
+rem                 3 2 1
 
 for /f "usebackq tokens=*" %%a in (`echor.cmd --edit-self -es /es :es`) do if /i "%~1" equ "%%a" goto EDIT_SELF
 
@@ -9,7 +28,10 @@ setlocal EnableDelayedExpansion
 set COUNT=%~1
 set /a COUNT+=0
 
-if %COUNT% lss 1 goto SHOW_HELP
+if %COUNT% lss 1 (
+    call bshelp.cmd "%~f0"
+    exit /b 1
+)
 
 set RESULT=
 set DEPTH=0
@@ -39,31 +61,3 @@ exit /b 0
 
 call code.cmd "%~f0"
 exit /b 0
-
-:SHOW_HELP
-
-echo -------------------------------------------------------------------------------
-echo Generate All Sort Orders
-echo -------------------------------------------------------------------------------
-echo.
-echo usage:
-echo.
-echo     %~n0[%~x0] number-of-elements
-echo.
-echo example:
-echo.
-echo     %~n0 2
-echo.
-echo         1 2
-echo         2 1
-echo.
-echo     %~n0 3
-echo.
-echo         1 2 3
-echo         1 3 2
-echo         2 1 3
-echo         2 3 1
-echo         3 1 2
-echo         3 2 1
-
-exit /b 1
