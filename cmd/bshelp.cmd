3992c2fbbfab1257140ccc8160007a87e9e0438b
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 03:38:24 +0900

    cmd/bshelp.cmd: specification change: help rows default preamble is `rem` only.

--- cmd/bshelp.cmd
+++ cmd/bshelp.cmd
@@ -2,20 +2,26 @@
 @rem Batch Script Help Part Displayer
 @rem -------------------------------------------------------------------------------
 @rem usage:
-@rem     bshelp[.cmd] source
+@rem     bshelp[.cmd] source [preamble]
 @rem arguments:
 @rem     source:
 @rem         batch script file path.
+@rem     preamble:
+@rem         help row preamble. (default `rem`)
 
 @setlocal
 
 @set SOURCE=%~1
-@if not defined SOURCE call "%~f0" "%~f0" & exit /b 1
+@if not defined SOURCE call "%~f0" "%~f0" "@rem" & exit /b 1
+
+@set PREAMBLE=%~2
+@if not defined PREAMBLE set PREAMBLE=rem
 
 @set PSCMD=^
+$preamble = [System.Text.RegularExpressions.Regex]::Escape('%PREAMBLE%'); ^
 Get-Content -LiteralPath '%SOURCE%' -Encoding OEM ^| ^
 ForEach-Object { ^
-if ($_ -match '^^\s*@?rem( ?(?^<HelpMessage^>.*))') { ^
+if ($_ -match """^^\s*$preamble( ?(?^<HelpMessage^>.*))""") { ^
 Write-Output $Matches['HelpMessage'] ^
 } ^
 }
