5aaaf3a4ca05b8b753261af85cb19379e3b65b8c
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 01:49:07 +0900

    cmd/lluid.cmd: added.

---
+++ cmd/lluid.cmd
@@ -0,0 +1,20 @@
+::? -------------------------------------------------------------------------------
+::? Local L[ax|oose] Unique ID (nearly timestamp)
+::? -------------------------------------------------------------------------------
+::? usage:
+::?     lluid[.cmd] [retvar]
+::? format:
+::?     yyyymmdd.hhmmss.ff.nnnnn 
+::? example:
+::?     lluid
+::?       or
+::?     lluid ID >nul
+::?     echo %ID%
+
+@setlocal
+@set /a LLUID=100000+%RANDOM%
+@set LLUID=%DATE:/=%.%TIME::=%.%LLUID:~1%
+@set LLUID=%LLUID:-=%
+@set LLUID=%LLUID: =0%
+@echo %LLUID%
+@endlocal & if "%~1" neq "" set %~1=%LLUID%
