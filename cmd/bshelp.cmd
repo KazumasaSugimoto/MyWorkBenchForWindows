3d9d9e8f5c87f1acf76eb938d0087f8310dd48a8
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 03:20:38 +0900

    cmd/bshelp.cmd: minor change.

--- cmd/bshelp.cmd
+++ cmd/bshelp.cmd
@@ -1,7 +1,19 @@
+@rem -------------------------------------------------------------------------------
 @rem Batch Script Help Part Displayer
+@rem -------------------------------------------------------------------------------
+@rem usage:
+@rem     bshelp[.cmd] source
+@rem arguments:
+@rem     source:
+@rem         batch script file path.
+
 @setlocal
+
+@set SOURCE=%~1
+@if not defined SOURCE call "%~f0" "%~f0" & exit /b 1
+
 @set PSCMD=^
-Get-Content -LiteralPath '%~1' -Encoding OEM ^| ^
+Get-Content -LiteralPath '%SOURCE%' -Encoding OEM ^| ^
 ForEach-Object { ^
 if ($_ -match '^^\s*@?rem( ?(?^<HelpMessage^>.*))') { ^
 Write-Output $Matches['HelpMessage'] ^
