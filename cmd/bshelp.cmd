e2af27db127cc6ae399f33b1ade9b8832deec41d
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 02:37:20 +0900

    cmd/bshelp.cmd: removed dependency on external `.ps1` script.

--- cmd/bshelp.cmd
+++ cmd/bshelp.cmd
@@ -2,6 +2,14 @@
 
 rem Batch Script Help Part Displayer
 
+setlocal
+set PSCMD=^
+Get-Content -LiteralPath '%~1' -Encoding OEM ^| ^
+ForEach-Object { ^
+if ($_ -match '^^\s*@?rem( ?(?^<HelpMessage^>.*))') { ^
+Write-Output $Matches['HelpMessage'] ^
+} ^
+}
 :: don't use `ps.cmd` because response speed is priority.
-powershell -NoProfile -File "%~dpn0.ps1" %*
+powershell -NoProfile -Command "%PSCMD%"
 exit /b %ERRORLEVEL%
