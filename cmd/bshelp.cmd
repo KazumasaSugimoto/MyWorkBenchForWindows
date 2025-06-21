75fcaf55303bb7bd45850382d0dcdc99fac0025d
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 03:10:50 +0900

    cmd/bshelp.cmd: countermeasure to `echo off` side effect for caller side.

--- cmd/bshelp.cmd
+++ cmd/bshelp.cmd
@@ -1,9 +1,6 @@
-@echo off
-
-rem Batch Script Help Part Displayer
-
-setlocal
-set PSCMD=^
+@rem Batch Script Help Part Displayer
+@setlocal
+@set PSCMD=^
 Get-Content -LiteralPath '%~1' -Encoding OEM ^| ^
 ForEach-Object { ^
 if ($_ -match '^^\s*@?rem( ?(?^<HelpMessage^>.*))') { ^
@@ -11,5 +8,5 @@ Write-Output $Matches['HelpMessage'] ^
 } ^
 }
 :: don't use `ps.cmd` because response speed is priority.
-powershell -NoProfile -Command "%PSCMD%"
-exit /b %ERRORLEVEL%
+@powershell -NoProfile -Command "%PSCMD%"
+@exit /b %ERRORLEVEL%
