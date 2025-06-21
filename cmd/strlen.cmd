6073b2f09f67ed705d2b99fe2101f1cfe7939461
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-22 01:32:04 +0900

    cmd/strlen.cmd: added.

---
+++ cmd/strlen.cmd
@@ -0,0 +1,26 @@
+::? -------------------------------------------------------------------------------
+::? String Length
+::? -------------------------------------------------------------------------------
+::? usage:
+::?     strlen[.cmd] string [retvar]
+::? example:
+::?     strlen "abc"
+::?       or
+::?     strlen "abc" LEN >nul
+::?     echo %LEN%
+
+@setlocal EnableDelayedExpansion
+@set LENGTH=0
+@set STRING=%~1
+@if not defined STRING goto PUT_RESULT
+
+:CHECK_NEXT_CHAR
+
+@if "!STRING:~%LENGTH%!" equ "" goto PUT_RESULT
+@set /a LENGTH+=1
+@goto CHECK_NEXT_CHAR
+
+:PUT_RESULT
+
+@echo %LENGTH%
+@endlocal & if "%~2" neq "" set %~2=%LENGTH%
