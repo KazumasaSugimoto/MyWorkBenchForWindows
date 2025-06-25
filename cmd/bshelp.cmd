15daba132f59a79387a4283876fd0c9bd8899449
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-25 22:01:55 +0900

    cmd/bshelp.cmd: added extension check function.

--- cmd/bshelp.cmd
+++ cmd/bshelp.cmd
@@ -17,6 +17,12 @@
 @set PREAMBLE=%~2
 @if not defined PREAMBLE set PREAMBLE=rem
 
+@call :IS_BATCH_FILE "%SOURCE%"
+@if ERRORLEVEL 1 (
+    echo "%SOURCE%" is not batch file.>&2
+    exit /b 1
+)
+
 @if defined USE_OLD_VERSION goto OLD_VERSION
 
 :NEW_VERSION
@@ -50,3 +56,8 @@ Write-Output ('{0}' -f $Matches['HelpMessage']) ^
 :: don't use `ps.cmd` because response speed is priority.
 @powershell -NoProfile -Command "%PSCMD%"
 @exit /b %ERRORLEVEL%
+
+:IS_BATCH_FILE
+
+@for /f "usebackq tokens=*" %%x in (`echor.cmd .bat .cmd`) do @if /i "%~x1" equ "%%x" exit /b 0
+@exit /b 1
