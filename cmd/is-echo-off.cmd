2ca90812759866da53214462d4d74032fa710e9f
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-26 21:26:57 +0900

    cmd/is-echo-off.cmd: bug, typo, minor fixs.

--- cmd/is-echo-off.cmd
+++ cmd/is-echo-off.cmd
@@ -4,27 +4,27 @@
 ::? usage:
 ::?     is-echo-off[.cmd]
 ::? errorlevel:
-::?     0: true (`echo off`) | 1: false (`echo on`)
+::?     0: true (echo is off) | 1: false (echo is on)
 @setlocal
 @call lluid.cmd LLUID >nul
-@set TEMP_FILE_PATH=%TEMP%\%~nx0.result.tmp
-@echo >"%TEMP_FILE_PATH%"
+@set ECHO_STATE=%TEMP%\%~nx0.%LLUID%.result.tmp
+@echo >"%ECHO_STATE%"
 ::? tested code pages:
 ::?     932   - Shift JIS
 ::?     65001 - UTF-8
-@findstr /IL "off. <OFF>" "%TEMP_FILE_PATH%" >nul 2>&1 && (set RESULT=0) || (set RESULT=1)
-@del "%TEMP_FILE_PATH%"
+@findstr /IL "off. <OFF>" "%ECHO_STATE%" >nul 2>&1 && (set RESULT=0) || (set RESULT=1)
+@del "%ECHO_STATE%"
 @if "%~1" neq "" (
     call bshelp.cmd "%~f0" "::?"
     echo result:
     echo     %RESULT%
 )
 @endlocal & exit /b %RESULT%
-::! implemantation caution:
-::!     NG: echo | findstr ... -> always 'on'.
-::!     OK: echo >"temp-file" & findstr ... "temp-file"
+::! implementation caution:
+::!     NG: `echo | findstr ...` -> always 'on'.
+::!     OK: `echo >"temp-file" & findstr ... "temp-file"`
 ::!     verification steps:
-::!         1. echo on  & echo | more
-::!         2. echo off & echo | more
-::!         3. echo on  & echo >dummy.txt & more dummy.txt
-::!         4. echo off & echo >dummy.txt & more dummy.txt
+::!         1. `echo on  & echo | more`
+::!         2. `echo off & echo | more`
+::!         3. `echo on  & echo >dummy.txt & more dummy.txt`
+::!         4. `echo off & echo >dummy.txt & more dummy.txt`
