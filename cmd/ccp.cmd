b37c6704a4091b417acb327ff27d1443935649b6
Kazumasa Sugimoto (GPD P2Max) <16056772+KazumasaSugimoto@users.noreply.github.com>
2025-06-27 21:35:23 +0900

    cmd/ccp.cmd: minor fix.

--- cmd/ccp.cmd
+++ cmd/ccp.cmd
@@ -8,7 +8,8 @@
 @set TEMP_FILE_PATH=%TEMP%\%~nx0.%LLUID%.result.tmp
 @chcp >"%TEMP_FILE_PATH%"
 @set /p TEMP_FILE_ROW=<"%TEMP_FILE_PATH%"
+@set RESULT=
 @for /f "usebackq tokens=*" %%a in (`echor.cmd %TEMP_FILE_ROW%`) do @set RESULT=%%a
-@echo %RESULT%
+@echo.%RESULT%
 @del "%TEMP_FILE_PATH%"
 @endlocal & if "%~1" neq "" set %~1=%RESULT%
